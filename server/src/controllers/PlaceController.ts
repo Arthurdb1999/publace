d0689b0f0b65f0b050b0ddd59528b36b1caa97a9
Douglas Vieira <douglasalissonvieira@gmail.com>
2021-04-11 21:27:11 -0300

    Add: Images / querry postgis

--- server/src/controllers/PlaceController.ts
+++ server/src/controllers/PlaceController.ts
@@ -6,10 +6,14 @@ export default class PlaceController {
     async index(req: Request, res: Response) {
 
         const { latitude, longitude } = req.query
-
+        console.log(latitude, longitude);
+        
         const { rows: places } = await db.query(`
-            SELECT ST_X(geom) as longitude, ST_Y(geom) as latitude, * from place
+            SELECT ST_X(geom) as longitude, ST_Y(geom) as latitude, *,
+            ST_Distance(ST_SetSRID(geom, 4618), ST_SetSRID(ST_MakePoint(${latitude}, ${longitude}), 4618)) * 100 as km from place,
+            ST_Buffer(ST_Transform(ST_SetSRID(ST_MakePoint(${latitude}, ${longitude}),4618), 26986), 3000,'quad_segs=360')
             `)
+            
         // INNER JOIN place_usuario ON place_usuario.place_id = place.id
         // INNER JOIN usuario ON usuario.id = place_usuario.usuario_id
         // INNER JOIN relation ON relation.id = place_usuario.relation_id
